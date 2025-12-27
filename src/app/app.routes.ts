81fd581624cf85998792a69001535e426af8c462
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2025-12-27 13:04:08 +0100

    feat: update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,31 @@ import { TransactionsComponent } from './components/transactions/transactions.co
 import { TransactionEditComponent } from './components/transactions/transaction-edit/transaction-edit.component';
 
 export const routes: Routes = [
-    { path: '', component: TransactionsComponent },
+    {
+        path: '',
+        pathMatch: 'full',
+        component: TransactionsComponent
+    },
     { path: 'transactions/edit/:id', component: TransactionEditComponent },
+    {
+        path: 'add',
+        loadComponent: () => {
+            return import('./components/transactions/transaction-add/transaction-add.component').then(m => m.TransactionAddComponent);
+        }
+    },
+    {
+        path: 'transactions',
+        loadComponent: () => {
+            return import('./components/transactions/transaction-list/transaction-list.component').then(m=>m.TransactionListComponent);
+        }
+
+    },
+    {
+        path: 'chart',
+        loadComponent: () => {
+            return import('./components/transactions/transaction-chart/transaction-chart.component').then(m => m.TransactionChartComponent);
+        }
+    }
 ];
 
 @NgModule({
