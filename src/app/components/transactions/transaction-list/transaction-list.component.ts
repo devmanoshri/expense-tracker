8ba862da0997a8a4ed6041ba665a5fa32674e4ee
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2025-12-31 18:11:26 +0100

    add sorting in list transaction component

--- src/app/components/transactions/transaction-list/transaction-list.component.ts
+++ src/app/components/transactions/transaction-list/transaction-list.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Category } from '../../../models/category.model';
@@ -8,20 +15,19 @@ import { CategoryStoreService } from '../../../services/category-store.service';
 import { TransactionStoreService } from '../../../services/transaction-store.service';
 import { TransactionService } from '../../../services/transaction.service';
 
-
 @Component({
   selector: 'app-transaction-list',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './transaction-list.component.html',
-  styleUrls: ['./transaction-list.component.scss']
+  styleUrls: ['./transaction-list.component.scss'],
 })
-
-export class TransactionListComponent implements OnInit {
-
+export class TransactionListComponent implements OnInit, OnChanges {
   //transactions$!: Observable<Transaction[]>;
   categories$!: Observable<Category[]>;
   @Input() transactions: Transaction[] | undefined;
+  sortOrder: 'asc' | 'desc' = 'desc';
+  sortedTransactions: Transaction[] = [];
 
   private categoryStoreServices = inject(CategoryStoreService);
   private transactionStoreServices = inject(TransactionStoreService);
@@ -33,6 +39,13 @@ export class TransactionListComponent implements OnInit {
 
     //this.transactions$ = this.transactionStoreServices.transactions$;
     this.categories$ = this.categoryStoreServices.categories$;
+    this.applySorting();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['transactions']) {
+      this.applySorting();
+    }
   }
 
   getCategoryName(id?: string): string {
@@ -40,10 +53,29 @@ export class TransactionListComponent implements OnInit {
   }
 
   onDelete(selectedTransactionId: string): void {
-    const confirmDelete = confirm('Are you sure you want to delete this transaction?');
+    const confirmDelete = confirm(
+      'Are you sure you want to delete this transaction?'
+    );
     if (!confirmDelete) return;
 
-    this.transactionService.deleteTransaction(selectedTransactionId)
+    this.transactionService
+      .deleteTransaction(selectedTransactionId)
       .subscribe(() => this.transactionStoreServices.initTransaction(true));
+  }
+
+  onSelectSorting(event: Event) {
+    this.sortOrder = (event.target as HTMLSelectElement).value as
+      | 'asc'
+      | 'desc';
+    this.applySorting();
+  }
+
+  applySorting() {
+    if (!this.transactions) return;
+    this.sortedTransactions = [...this.transactions].sort((a, b) =>
+      this.sortOrder === 'desc'
+        ? new Date(b.date).getTime() - new Date(a.date).getTime()
+        : new Date(a.date).getTime() - new Date(b.date).getTime()
+    );
   }
 }
--- src/app/pipes/filter-transaction.pipe.ts
+++ src/app/pipes/filter-transaction.pipe.ts
@@ -19,26 +19,31 @@ export class FilterTransactionPipe implements PipeTransform {
   ): Transaction[] {
     const { transactionType, selectedCategoryId, fromDate, toDate } =
       filterOptions;
+
     if (!transactionType && !selectedCategoryId && !fromDate && !toDate) {
       return transactions;
     }
 
-    if (selectedCategoryId === '') {
-      return transactions.filter(
-        (transaction) => transaction.type === transactionType
-      );
-    }
-
     return transactions.filter((transaction) => {
-      const transactionDate = new Date(transaction.date).getTime();
-      const from = fromDate ? new Date(fromDate).getTime() : null;
-      const to = toDate ? new Date(toDate).getTime() : null;
-
-      if (transaction.categoryId !== selectedCategoryId) {
+      //  Filter by type
+      if (transactionType && transaction.type !== transactionType) {
         return false;
       }
-      if (from && transactionDate < from) return false;
-      if (to && transactionDate > to) return false;
+
+      // Filter by category (only for expenses)
+      if (
+        transaction.type === 'expense' &&
+        selectedCategoryId &&
+        transaction.categoryId !== selectedCategoryId
+      ) {
+        return false;
+      }
+
+      // Filter by date range
+      const transactionDate = new Date(transaction.date).getTime();
+      if (fromDate && transactionDate < new Date(fromDate).getTime())
+        return false;
+      if (toDate && transactionDate > new Date(toDate).getTime()) return false;
 
       return true;
     });
