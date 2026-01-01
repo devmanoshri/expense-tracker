07b0c84315aa542b17524a6727d9dff31926a58d
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-01 13:21:45 +0100

    feat: update router with new folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TransactionsComponent } from './components/transactions/transactions.component';
-import { TransactionEditComponent } from './components/transactions/transaction-edit/transaction-edit.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        component: TransactionsComponent
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent,
+  },
+  {
+    path: 'transactions/:id',
+    loadComponent: () => {
+      return import('./components/shared/transaction-edit/transaction-edit.component').then(
+        (m) => m.TransactionEditComponent,
+      );
     },
-    { path: 'transactions/:id', component: TransactionEditComponent },
-    {
-        path: 'transaction',
-        loadComponent: () => {
-            return import('./components/transactions/transaction-edit/transaction-edit.component').then(m => m.TransactionEditComponent);
-        }
+  },
+  {
+    path: 'add-transactions',
+    loadComponent: () => {
+      return import('./components/shared/transaction-edit/transaction-edit.component').then(
+        (m) => m.TransactionEditComponent,
+      );
     },
-    {
-        path: 'transactions',
-        loadComponent: () => {
-            return import('./components/transactions/transaction-list/transaction-list.component').then(m=>m.TransactionListComponent);
-        }
-
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => {
+      return import('./components/pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent,
+      );
     },
-    {
-        path: 'chart',
-        loadComponent: () => {
-            return import('./components/transactions/transaction-chart/transaction-chart.component').then(m => m.TransactionChartComponent);
-        }
-    }
-]; 
+  },
+  {
+    path: 'manage-transaction',
+    loadComponent: () => {
+      return import('./components/pages/manage-transaction/manage-transaction.component').then(
+        (m) => m.ManageTransactionComponent,
+      );
+    },
+  },
+  {
+    path: 'chart-preview',
+    loadComponent: () => {
+      return import('./components/pages/chart-preview/chart-preview.component').then(
+        (m) => m.ChartPreviewComponent,
+      );
+    },
+  },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
