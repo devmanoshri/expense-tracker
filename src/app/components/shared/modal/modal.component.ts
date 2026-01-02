b729f5d7bfbaa0eec45583f2cf8d66a11bc38fb8
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-02 13:05:52 +0100

    feat: add modal component

---
+++ src/app/components/shared/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  @Input() header = '';
+  @Output() modalClose = new EventEmitter<void>();
+}
