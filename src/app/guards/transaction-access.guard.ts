f610c65f265cf542e03f50771e0674967866bfd7
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-13 00:34:50 +0100

    feat: update location

--- src/app/guards/transaction-access.guard.ts
+++ src/app/guards/transaction-access.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { MessageService } from '../../shared/message/message.service';
+import { MessageService } from '../components/shared/message/message.service';
 
 export const transactionAccessGuard: CanActivateFn = (route, state) => {
   const messageService = inject(MessageService);
